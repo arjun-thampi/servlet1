9b8cc77dcf8c9fe145b14d05dd3df67df43d2970
Arjun S Thampi <arjunsthampi99@gmail.com>
2022-03-23 21:46:17 +0530

    cookies file added

---
+++ src/cook/LoginServlet.java
@@ -0,0 +1,65 @@
+package cook;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+//@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter p =response.getWriter();
+		
+		String user=request.getParameter("t1");
+		String pwd=request.getParameter("p1");
+		
+		if((user.equals("cdac"))&&(pwd.equals("stdc")))
+		{
+			request.setAttribute("Userid", 15);
+			RequestDispatcher rd=request.getRequestDispatcher("welcome");
+			rd.forward(request,response);
+			
+		}
+		else
+		{
+			p.println("invalid");
+			p.println("enter correct username and password");
+			RequestDispatcher rd=request.getRequestDispatcher("login.html");
+			rd.include(request,response);
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/cook/ProfileServlet.java
@@ -0,0 +1,64 @@
+package cook;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProfileServlet
+ */
+//@WebServlet("/ProfileServlet")
+public class ProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProfileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		PrintWriter p =response.getWriter();
+		
+		RequestDispatcher rd=request.getRequestDispatcher("link.html");
+		rd.include(request,response);
+		
+		Cookie ckarray[]=request.getCookies();
+		if(ckarray!=null)
+		{
+			p.println("profiles details of"+ckarray[0].getValue());
+		}
+		else
+		{
+			p.println("login first");
+			RequestDispatcher rd1=request.getRequestDispatcher("login.html");
+			rd1.include(request,response);
+		}
+	
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
